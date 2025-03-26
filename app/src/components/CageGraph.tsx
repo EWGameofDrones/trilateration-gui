1009fe1effcedf47eafe36e3ca83181fd6960895
EWGameOfDrones <ewgameofdrones@gmail.com>
2025-03-26 08:49:37 -0700

    fixed drone size scaling

--- app/src/components/CageGraph.tsx
+++ app/src/components/CageGraph.tsx
@@ -130,24 +130,6 @@ export const CageGraph: Component<{}> = () => {
     window.removeEventListener('resize', handleResize)
   })
 
-  async function demoDroneMovement() {
-    while (true) {
-      parsePacket({
-        x: Math.random() * cageLength(),
-        y: Math.random() * cageWidth(),
-        id: 0,
-      })
-      parsePacket({
-        x: 40,
-        y: 20,
-        id: 1,
-      })
-      await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000))
-    }
-  }
-
-  demoDroneMovement()
-
   return (
     <>
       <div
@@ -180,7 +162,7 @@ export const CageGraph: Component<{}> = () => {
           <DroneDisplay
             xScale={getXScale()}
             yScale={getYScale()}
-            droneSize={droneProportion * graphicHeight()}
+            droneSize={droneProportion * getYScale().range()[1]}
             showPaths={true}
           />
         </svg>
